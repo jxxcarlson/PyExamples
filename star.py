3b2fc38e2fa40fb1ad306df9dfaeb52661bf4a5e
James Carlson <jxxcarlson@mac.com>
2013-02-21 05:55:43 -0500

    Update star.py

--- star.py
+++ star.py
@@ -8,6 +8,9 @@
 # Language: Python 3.3
 # Author: J. Carlson
 # Date: 2/19/2013
+#
+# REQUIRES: utilities.py -- from this repository
+#
 # Site for images: square-the-circle.com
 
 import sys 
@@ -24,7 +27,7 @@ def star(turtle, n,r):
     turtle.backward(r)
     turtle.left(360/n)
 
-f = 0.3
+f = 0.3  # rescaling factor
     
 def recursive_star(turtle, n, r, depth):
   """At each point of the star, draw another smaller star,
@@ -47,10 +50,13 @@ fred = Turtle()
 fred.speed("fastest")
 # star(fred, 20, 100) # draw a 20-star
 
-# Draw a fractal cross of depth sys.argv[1]:
+# Draw a fractal star of depth sys.argv[2] with sys.argv[1] rays:
 recursive_star(fred, int(sys.argv[1]), 200, int(sys.argv[2]))
 fred.hideturtle()
+
+# If there are enough arguments, save the image:
 if len(sys.argv) == 4:
   saveImage(fred, sys.argv[3]+sys.argv[2]+".eps")
+  
 exitonclick()  
     
